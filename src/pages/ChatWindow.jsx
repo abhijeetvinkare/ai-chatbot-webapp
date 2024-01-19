14c638649e991c1d39a45388bc7d5d41f80760e7
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2024-01-19 15:45:48 +0530

    initial commit

--- src/pages/ChatWindow.jsx
+++ src/pages/ChatWindow.jsx
@@ -5,28 +5,51 @@ function ChatWindow() {
   return (
     <div className="p-3 flex flex-col justify-between h-full space-y-4">
       <div className="overflow-y-scroll px-8 py-4 text-justify space-y-10 flex flex-col">
-        <p className="bg-[#1A2232] p-2 max-w-2xl self-end ml-auto">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
-          voluptas explicabo quasi corporis!
-        </p>
-        <p className="bg-[#1A2232] p-2 max-w-2xl self-start">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
-          voluptas explicabo quasi corporis! Sit ex vero accusamus veniam,
-          blanditiis minima! Maiores facilis, aspernatur quo laborum ratione
-          cupiditate dignissimos unde repellat aliquid dolor nesciunt dolorum
-          inventore maxime quisquam est architecto veritatis pariatur dolores
-          voluptatum. 
-        </p>
-        <p className="bg-[#1A2232] p-2 max-w-2xl self-end ml-auto">
-          Lorem ipsum dolor sit
-        </p>
-        <p className="bg-[#1A2232] p-2 w-full text-left max-w-96 self-start">
-          Lorem ipsum dolor sit amet consectetu.
-        </p>
-        <p className="bg-[#1A2232] p-2 max-w-2xl self-end ml-auto">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
-          voluptas explicabo quasi corporis! Sit ex vero accusamus veniam,
-        </p>
+        <div className="p-2 max-w-2xl self-start ml-auto">
+          <span className="text-end block w-full text-xs mb-2 text-gray-500">
+            2:03 PM, 15 Nov
+          </span>
+          <p className="bg-[#1A2232] p-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
+            voluptas explicabo quasi corporis!
+          </p>
+        </div>
+        <div className="p-2 max-w-2xl self-start">
+          <span className="text-start block w-full text-xs mb-2 text-gray-500">
+            2:03 PM, 15 Nov
+          </span>
+          <p className="bg-[#1A2232] p-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
+            voluptas explicabo quasi corporis! Sit ex vero accusamus veniam,
+            blanditiis minima! Maiores facilis, aspernatur quo laborum ratione
+            cupiditate dignissimos unde repellat aliquid dolor nesciunt dolorum
+            inventore maxime quisquam est architecto veritatis pariatur dolores
+            voluptatum.
+          </p>
+        </div>
+        <div className="p-2 max-w-2xl self-start ml-auto">
+          <span className="text-end block w-full text-xs mb-2 text-gray-500">
+            2:03 PM, 15 Nov
+          </span>
+          <p className="bg-[#1A2232] p-2">Lorem ipsum dolor sit</p>
+        </div>
+        <div className="p-2 max-w-2xl self-start">
+          <span className="text-start block w-full text-xs mb-2 text-gray-500">
+            2:03 PM, 15 Nov
+          </span>
+          <p className="bg-[#1A2232] p-2">
+            Lorem ipsum dolor sit amet consectetu.
+          </p>
+        </div>
+        <div className="p-2 max-w-2xl self-start ml-auto">
+          <span className="text-end block w-full text-xs mb-2 text-gray-500">
+            2:03 PM, 15 Nov
+          </span>
+          <p className="bg-[#1A2232] p-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
+            voluptas explicabo quasi corporis! Sit ex vero accusamus veniam.
+          </p>
+        </div>
       </div>
       <div className="pt-2 pb-3 px-8">
         <form action="/search" className="w-full">
